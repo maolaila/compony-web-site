f8cdeb118588be672b9d18a2a328cf92da8ffcb7
pony <pony@123.com>
2025-11-28 09:41:40 +0900

    fix github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // 这里设置 ./ 是为了让部署在任何子路径下都能正常访问
-  base: './' 
+  // GitHub Pages 部署需要设置为仓库名称
+  base: '/compony-web-site/'
 })
